be77db4f64774748caa992dd45469c8b21b6e948
Riajul Islam <javascriptForEverything@gmail.com>
2024-04-18 15:08:17 +0600

    Google Font: add roboto font with tailwind-intellegence

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,82 @@ module.exports = {
 			}
 		},
   },
-  plugins: [],
+  plugins: [
+		({ addUtilities, addComponents }) => {
+			addUtilities({
+				'.roboto-thin': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '100',
+					fontStyle: 'normal'
+				},
+
+				'.roboto-light': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '300',
+					fontStyle: 'normal'
+				},
+
+				'.roboto-regular': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '400',
+					fontStyle: 'normal'
+				},
+
+				'.roboto-medium': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '500',
+					fontStyle: 'normal'
+				},
+
+				'.roboto-bold': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '700',
+					fontStyle: 'normal'
+				},
+
+				'.roboto-black': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '900',
+					fontStyle: 'normal'
+				},
+
+				'.roboto-thin-italic': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '100',
+					fontStyle: 'italic'
+				},
+
+				'.roboto-light-italic': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '300',
+					fontStyle: 'italic'
+				},
+
+				'.roboto-regular-italic': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '400',
+					fontStyle: 'italic'
+				},
+
+				'.roboto-medium-italic': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '500',
+					fontStyle: 'italic'
+				},
+
+				'.roboto-bold-italic': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '700',
+					fontStyle: 'italic'
+				},
+
+				'.roboto-black-italic': {
+					fontFamily: ["Roboto", 'sans-serif'],
+					fontWeight: '900',
+					fontStyle: 'italic'
+				},
+			})
+		}
+	],
 }
 
